057f86c3129a57964e42ff78ee0c0134b2e1bda1
apa-afk <pakin.alexandre@gmail.com>
2025-12-26 18:44:20 +0100

    get_raw_data_acc

--- modules/module_data.py
+++ modules/module_data.py
@@ -2,6 +2,17 @@ import requests
 import pandas as pd
 
 
+def get_raw_data_ACC(api_url):
+    """
+    Calls a public API to retrieve data in DataFrame format.
+    Parameters:
+    - api_url (str): The URL of the API.
+    Returns:
+    DataFrame: The data in DataFrame format.
+    """
+    response = requests.get(api_url)
+    data = response.json()
+    return pd.DataFrame(data["data"]) # deletes the pagination
 
 
 def get_raw_data(api_url):
